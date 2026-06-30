4f1c7b275b838309cde17b3c7751d4e92fb16fab
bbyshwv <bbyshwv@gmail.com>
2026-06-30 15:00:25 +0700

    update

---
+++ MaxVerse.API/Authentication/Interfaces/IJwtService.cs
@@ -0,0 +1,10 @@
+﻿using MaxVerse.API.Models.Users;
+
+namespace MaxVerse.API.Authentication.Interfaces;
+
+public interface IJwtService
+{
+    string GenerateAccessToken(User user);
+
+    string GenerateRefreshToken();
+}
---
+++ MaxVerse.API/DTOs/Auth/AuthResponse.cs
@@ -0,0 +1,16 @@
+﻿namespace MaxVerse.API.DTOs.Auth;
+
+public class AuthResponse
+{
+    public string AccessToken { get; set; } = string.Empty;
+
+    public string RefreshToken { get; set; } = string.Empty;
+
+    public DateTime ExpireAt { get; set; }
+
+    public string FullName { get; set; } = string.Empty;
+
+    public string Email { get; set; } = string.Empty;
+
+    public string Role { get; set; } = string.Empty;
+}
